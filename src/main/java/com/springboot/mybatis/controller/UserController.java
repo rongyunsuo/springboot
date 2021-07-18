f39bb25f6a8918cc94d53049ddeedafc24c48dd3
rongyunsuo <1445574013@qq.com>
2021-07-18 11:21:16 +0800

    mybatis and redis

--- src/main/java/com/springboot/mybatis/controller/UserController.java
+++ src/main/java/com/springboot/mybatis/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/mybatis")
 public class UserController {
 
     @Autowired
--- src/main/java/com/springboot/redis/controller/RedisController.java
+++ src/main/java/com/springboot/redis/controller/RedisController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
+@RequestMapping("/redis")
 public class RedisController {
     @Autowired
     RedisUtil redisUtil;
@@ -14,7 +17,8 @@ public class RedisController {
     @RequestMapping("/set")
     public String set() {
         RedisUser redisUser = new RedisUser(1, "david", "123");
-        redisUtil.set("user", redisUser);
+        int i = new Random().nextInt(100);
+        redisUtil.set("user" + i, redisUser);
         return "success saved user !";
     }
 
